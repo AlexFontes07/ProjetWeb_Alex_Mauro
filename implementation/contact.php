41a66582fa5d08c25170583de4750c9df912244b
Alexandre.FONTES <alexandre.fontes@cpnv.ch>
2019-03-06 12:08:06 +0100

    MAJ Contact

--- implementation/contact.php
+++ implementation/contact.php
@@ -24,89 +24,37 @@ $titre="RentASnow - Accueil";
 				
 				<div class="col-md-8 contact-grid">
 					<form>	
-						<input type="text" value="Name" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='Name';}">
+						<input type="text" value="Nom" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='Nom';}">
 					
 						<input type="text" value="Email" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='Email';}">
-						<input type="text" value="Subject" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='Subject';}">
+						<input type="text" value="Sujet" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='Sujet';}">
 						
 						<textarea cols="77" rows="6" value=" " onfocus="this.value='';" onblur="if (this.value == '') {this.value = 'Message';}">Message</textarea>
 						<div class="send">
-							<input type="submit" value="Send">
+							<input type="submit" value="Envoyer">
 						</div>
 					</form>
 				</div>
 				<div class="col-md-4 contact-in">
 
 						<div class="address-more">
-						<h4>Address</h4>
-							<p>The company name,</p>
-							<p>Lorem ipsum dolor,</p>
-							<p>Glasglow Dr 40 Fe 72. </p>
+						<h4>Adresse</h4>
+							<p>BuyHard,</p>
+							<p>Avenue de la gare 14,</p>
+							<p>1450 Sainte-Croix. </p>
 						</div>
 						<div class="address-more">
-						<h4>Address1</h4>
-							<p>Tel:1115550001</p>
-							<p>Fax:190-4509-494</p>
-							<p>Email:<a href="mailto:contact@example.com"> contact@example.com</a></p>
-						</div>
+						<h4>Informations</h4>
+							<p>Tel: 078/699.99.99</p>
+							<p>Email:<a href="mailto:contact@cpnv.ch"> contact@cpnv.ch
 					
 				</div>
 				<div class="clearfix"> </div>
-			</div>
-			<div class="map">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37494223.23909492!2d103!3d55!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x453c569a896724fb%3A0x1409fdf86611f613!2sRussia!5e0!3m2!1sen!2sin!4v1415776049771"></iframe>
 			</div>
 		</div>
 		
 	</div>
 <!--//content-->
-<div class="footer">
-				<div class="container">
-			<div class="footer-top-at">
-			
-				<div class="col-md-4 amet-sed">
-				<h4>MORE INFO</h4>
-				<ul class="nav-bottom">
-						<li><a href="#">How to order</a></li>
-						<li><a href="#">FAQ</a></li>
-						<li><a href="contact.html">Location</a></li>
-						<li><a href="#">Shipping</a></li>
-						<li><a href="#">Membership</a></li>	
-					</ul>	
-				</div>
-				<div class="col-md-4 amet-sed ">
-				<h4>CONTACT US</h4>
-				
-					<p>
-Contrary to popular belief</p>
-					<p>The standard chunk</p>
-					<p>office:  +12 34 995 0792</p>
-					<ul class="social">
-						<li><a href="#"><i> </i></a></li>						
-						<li><a href="#"><i class="twitter"> </i></a></li>
-						<li><a href="#"><i class="rss"> </i></a></li>
-						<li><a href="#"><i class="gmail"> </i></a></li>
-						
-					</ul>
-				</div>
-				<div class="col-md-4 amet-sed">
-					<h4>Newsletter</h4>
-					<p>Sign Up to get all news update
-and promo</p>
-					<form>
-						<input type="text" value="" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='';}">
-						<input type="submit" value="Sign up">
-					</form>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-		</div>
-		<div class="footer-class">
-		<p >© 2015 New store All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
-		</div>
-		</div>
-</body>
-</html>
 <?php
 $contenu = ob_get_clean();
 require "gabarit.php";
