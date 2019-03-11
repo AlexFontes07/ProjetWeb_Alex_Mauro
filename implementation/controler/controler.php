37c744091a8ed8347179e14cdc0feec3a9022c29
Mauro-Alexandre.COST <maurx18@gmail.com>
2019-03-11 10:31:41 +0100

    Implémentation et MLD

---
+++ implementation/controler/controler.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mauro-Alexandre.COST
+ * Date: 11.03.2019
+ * Time: 08:14
+ */
+function openSingle_contact(){
+require "view/single_contact.php";
+}
+function openSingle(){
+    require "view/single.php";
+}
+function openRegister(){
+    require "view/register.php";
+}
+function openProducts(){
+    require "view/products.php";
+}
+function openLogin(){
+    require "view/login.php";
+}
+function openHome(){
+    require "view/home.php";
+}
+function openContact(){
+    require "view/contact.php";
+}
+
+?>
--- implementation/index.php
+++ implementation/index.php
@@ -1,107 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Pascal.BENZONANA
- * Date: 08.05.2017
- * Time: 09:16
- */
-// tampon de flux stocké en mémoire
-ob_start();
-$titre="RentASnow - Accueil";
-?><!--content-->
-<div class="banner">
-    <div class="container">
-        <script src="js/responsiveslides.min.js"></script>
-        <script>
-            $(function () {
-                $("#slider").responsiveSlides({
-                    auto: true,
-                    nav: true,
-                    speed: 500,
-                    namespace: "callbacks",
-                    pager: true,
-                });
-            });
-        </script>
-        <div  id="top" class="callbacks_container">
-            <ul class="rslides" id="slider">
-                <li>
-
-                    <div class="banner-text">
-                        <h3>Lorem Ipsum is not simply dummy  </h3>
-                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor .</p>
-                        <a href="single.html">Learn More</a>
-                    </div>
-
-                </li>
-                <li>
-
-                    <div class="banner-text">
-                        <h3>There are many variations </h3>
-                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor .</p>
-                        <a href="single.html">Learn More</a>
-
-                    </div>
-
-                </li>
-                <li>
-                    <div class="banner-text">
-                        <h3>Sed ut perspiciatis unde omnis</h3>
-                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor .</p>
-                        <a href="single.html">Learn More</a>
-
-                    </div>
-
-                </li>
-            </ul>
-        </div>
-
-    </div>
-</div>
-<div class="content">
-	<div class="container">
-	<div class="content-top">
-		<h1>NOS CATÉGORIES</h1>
-		<div class="grid-in">
-			<div class="col-md-4 grid-top">
-				<a href="single.php" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/pi.jpg" alt="">
-							<div class="b-wrapper">
-									<h3 class="b-animate b-from-left    b-delay03 ">
-										<span>Acheter</span>
-									</h3>
-								</div>
-				</a>
-		
-
-			<p><a href="single.php">Items achetables</a></p>
-			</div>
-			<div class="col-md-4 grid-top">
-				<a href="single.php" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/pi1.jpg" alt="">
-					<div class="b-wrapper">
-									<h3 class="b-animate b-from-left    b-delay03 ">
-										<span>Louer</span>
-									</h3>
-								</div>
-				</a>
-			<p><a href="single.php">Items louables</a></p>
-			</div>
-			<div class="col-md-4 grid-top">
-				<a href="single.php" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/pi2.jpg" alt="">
-					<div class="b-wrapper">
-									<h3 class="b-animate b-from-left    b-delay03 ">
-										<span>Services</span>
-									</h3>
-								</div>
-				</a>
-			<p><a href="single.php">Services Proposés</a></p>
-			</div>
-					<div class="clearfix"> </div>
-		</div>
-	</div>
-	<!----->
-</div>
-<?php
-$contenu = ob_get_clean();
-require "gabarit.php";
-
-			
+if(!isset($_SESSION)){
+    session_start();
+}
+require 'controler/controler.php';
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+    switch ($action) {
+        case "loginPage":
+            openLogin();
+            break;
+        case "RegisterPage":
+            openRegister();
+            break;
+        case "ProductsPage":
+            openProducts();
+            break;
+        case "ContactPage":
+            openContact();
+            break;
+        case "ContactPage":
+            openContact();
+            break;
+        default :
+            openHome();
+            break;
+    }
+} else {
+    openHome();
+}
+?>
